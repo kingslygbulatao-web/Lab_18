29f90ce84f7083154627aa512e0747d0b7cf909f
Me <planetaryaxis93@gmail.com>
2026-03-08 16:34:19 -0700

    yay2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+//struct cuz easier
 
 struct Node {
 
@@ -13,21 +14,49 @@ struct Node {
 
 Node * head;
 
+
+
 void InsertAtTheFront(Node**head, int newData){
 
     Node *newNode = new Node();
     newNode->data = newData;
 
+    newNode->next = *head;
 
+    *head = newNode;
+
+
+}
+
+void InsertAtTheBack(Node**head, int newData){
+
+    Node *newNode = new Node();
+    newNode->data = newData;
+    newNode->next = NULL;
+
+    if(*head == NULL){
+        *head = newNode;
+        return;
+    }
+    
+    Node* last = *head;
+
+    while (last->next != NULL){
+        last = last->next;
+    }
+
+    last->next = newNode;
 }
 
 int main(){
 
-    Node * head = nullptr;
+
+    Node* head = new Node();
 
     int LinklistChoice;
     int rating;
     string Comments;
+    int choice;
 
     cout << "Which linked list method method should we use?" << endl;
 
@@ -38,20 +67,37 @@ int main(){
 
     if(LinklistChoice == 1){
 
-        InsertAtTheFront(&head, -1)
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cout << "Enter review comments: ";
+        cin >> Comments;
+        cout << "Enter another review? 1 = y/2 = n: "; 
+        cin >> choice;
+
+        if(choice == 1){
+        cout << "Enter review rating 0-5: ";
+        cin >> rating;
+        cout << "Enter review comments: ";
+        cin >> Comments;
+        cout << "Enter another review? Y/N: "; 
+        cin >> choice;
+        }
+        else {
+            cout<< "grate..." << endl;
+        }
+
+
+        InsertAtTheFront(&head, -1);
 
     } 
-    if (LinklistChoice == 2){
+    else if (LinklistChoice == 2){
 
-
+        InsertAtTheBack(&head, 4);
 
     }
     else {
-        cout << "input a valid choice." << endl;
+        cout << "input a valid option" << endl;
     }
-
-
-    cout << "LinklistChoice: " << LinklistChoice << endl;
     
     
 }
